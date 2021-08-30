dc732307cfc5477ac5923468965013935912bc30
Trevor Sullivan <trevor@trevorsullivan.net>
2021-08-30 10:02:11 -0600

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -4,11 +4,12 @@ Set-Location -Path $PSScriptRoot
 
 $WebClient = [System.Net.WebClient]::new()
 $AWSPowerShellURL = 'https://sdk-for-net.amazonwebservices.com/ps/v4/latest/AWS.Tools.zip'
+$DestinationPath = '{0}/AWS.Tools.zip' -f $PSScriptRoot
 
 $WebClient.DownloadFile($AWSPowerShellURL, 'AWS.Tools.zip')
 
 # Extract the AWS PowerShell module
-Expand-Archive -Path ./AWS.Tools.zip -DestinationPath $env:PSModulePath.Split(':')[0]
+Expand-Archive -Path $DestinationPath -DestinationPath $env:PSModulePath.Split(':')[0]
 
 # Return the AWS Tools version from the script
 return (Get-Module -ListAvailable -Name AWS.Tools.Common).Version
